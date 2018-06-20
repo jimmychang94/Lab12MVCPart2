b725034d2e02c43e57a878b5c4f91b0cc1ab7aec
Jimmy Chang <jimmyfchang94@gmail.com>
2018-06-19 23:59:54 -0700

    follows the tutorial, 1 edit to css, some edits to html, adds README and images

---
+++ MVCMovie/MVCMovie/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCMovie.Models;
+using MVCMovie.Controllers;
+
+namespace MVCMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "A List of Movies?";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Contacts...Put them here if you have any!";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
